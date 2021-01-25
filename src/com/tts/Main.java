76a2485d64b4f2ad3ae79a9dbef9051c1118f45e
Lizziebeth76 <lizziebeth76@gmail.com>
2021-01-25 15:49:34 -0600

    first commit

---
+++ src/com/tts/Main.java
@@ -0,0 +1,16 @@
+package com.tts;
+
+
+public class Main {
+    // THIS "CODE" IS ALL ABOUT MESSING UP. INTENTIONALLY TRYING TO THROW ERRORS IN ORDER TO GET FAMILIAR WITH THE ERROR MESSAGES - VERY EASY TO DO! ;)
+    public static void main(String[]args) {
+        System.out.println("Hello World");
+    }
+        private int x;
+	    float myFloat;
+        int y=5;
+    private int = "hello";
+
+
+}
+
